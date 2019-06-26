754e211f3a18844d78f6e7a749346b602b5e76f4
Julio Caballero <173180@ids.upchiapas.edu.mx>
2019-06-26 10:32:33 -0500

    add

--- Alumno/serializer.py
+++ Alumno/serializer.py
@@ -2,7 +2,7 @@
 from rest_framework import routers, serializers, viewsets
 
 # -------------AGREGANDO MODELOS-----------------
-from alumno.models import Alumno
+from Alumno.models import Alumno
 
 class AlumnoSerializers(serializers.ModelSerializer):
     class Meta:
