061f7840d4a13f174ed76e4240d0ef6355746c7e
Jesus Marcelo Torres Pineda <173207@ids.upchiapas.edu.mx>
2019-06-28 21:02:58 -0500

    :c

--- Alumno/models.py
+++ Alumno/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
-
-class Alumnos(models.Model):
+class Alumno(models.Model):
+    token = models.IntegerField(null=False)
     name = models.CharField(max_length=254, null=False)
     lastName = models.CharField(max_length=254, null=False)
     matricula = models.CharField(max_length=254, null=False)
@@ -14,15 +13,4 @@ class Alumnos(models.Model):
         return self.name
 
     class Meta:
-        db_table = 'Alumnos'
-
-class Rfid(models.Model):
-    token = models.IntegerField(null=False)
-    id_alumno = models.ForeignKey(Alumnos, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'Rfid'
-
+        db_table = 'Alumno'
--- Alumno/views.py
+++ Alumno/views.py
@@ -8,7 +8,6 @@ from rest_framework import status
 from rest_framework import generics
 
 from Alumno.models import Alumno
-
 from Alumno.serializer import AlumnoSerializers
 
 class AlumnoList(APIView):
@@ -26,6 +25,35 @@ class AlumnoList(APIView):
             return Response(datas)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+class AlumnoDetail(APIView):
+    #Metodo get para solicitar info de un solo parametro
+    def get_object(self, id):
+        try:
+            return Alumno.objects.get(pk=id, delete = False)
+        except Alumno.DoesNotExist:
+            return 404
+
+    def get(self, id, request, format=None):
+        alumno = self.get_object(id)
+        if alumno != 404:
+            #many = True No aplica si retorno un solo objeto
+            serializer = AlumnoSerializers(alumno)
+            return Response(serializer.data)
+        else:
+            return Response(alumno)
+
+    def put(self, request, id, format=None):
+        alumno = self.get_object(id)
+        if alumno != 404:
+            serializer = AlumnoSerializers(alumno, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                datas = serializer.data
+                return Response(datas)
+            else:
+                return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        else: 
+            return Response(alumno, status = status.HTTP_404_BAD_REQUEST)
 
 
 # Create your views here.
